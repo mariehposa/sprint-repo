df6e66028244d670f540220cb24e5ab6e42cfed8
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-15 10:37:56 +0000

    create form to login

---
+++ client/src/components/login.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Form, Formik } from 'formik';
+import axios from 'axios';
+
+const initialValues = {
+    username: '',
+    password: ''
+}
+
+export default function Login () {
+    const toLogin = (formValues, actions) => {
+        axios.post('http://localhost:3300/api/auth/login', {
+          username: formValues.username,
+          password: formValues.password
+        })
+          .then( res => {
+            console.log(res)
+            localStorage.setItem('token', res.data.payload);
+            props.history.push('bubblePage')
+          })
+          .catch( err => {
+            alert(err.message)
+          })
+        actions.resetForm()
+    }
+
+    return (
+        <div>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={toLogin}
+                render = { props => {
+                    return (
+                      <Form>
+                          <Field name="username" placeholder="Enter username" type="text"/>
+                          <Field name="password" placeholder="Enter password" type="text"/>
+                          <button type="submit">Login</button>
+                      </Form>
+                    )
+                }}
+            />
+        </div>
+    );
+}
